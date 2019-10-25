58181df94563d24bbc44188f555d04aaf844efee
corn271828 <50471362+corn271828@users.noreply.github.com>
2019-10-25 07:43:03 -0500

    Create TankDriveHardware.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveHardware.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+public class TankDriveHardware
+{
+
+    public DcMotor  LM0  = null;
+    public DcMotor  LM1  = null;
+    public DcMotor  RM0  = null;
+    public DcMotor  RM1  = null;
+
+
+    HardwareMap hwMap =  null;
+
+    public ModuleHarware(){
+    }
+
+    public void init(HardwareMap ahwMap) {
+        hwMap = ahwMap;
+
+        M0 = hwMap.get(DcMotor.class, "M0");
+        M1 = hwMap.get(DcMotor.class, "M1");
+
+
+        M0.setDirection(DcMotor.Direction.FORWARD);
+        M1.setDirection(DcMotor.Direction.FORWARD);
+
+
+        M0.setPower(0);
+        M1.setPower(0);
+
+
+        M0.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        M1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+
+        M0.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        M1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    }
+}
+
