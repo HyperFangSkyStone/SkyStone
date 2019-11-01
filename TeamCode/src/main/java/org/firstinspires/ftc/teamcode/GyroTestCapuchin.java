b2b6d293717a20166a6e506615425536c35a71eb
Eric Y <ericyzt@hotmail.com>
2019-10-31 19:02:34 -0500

    Bug Fix
    Monkey mistakes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GyroTestCapuchin.java
@@ -0,0 +1,84 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import java.util.ArrayList;
+
+
+@TeleOp(name="Gyro Tester Capuchin", group="New World Apes")
+//@Disabled
+public class GyroTestCapuchin extends LinearOpMode {
+    public final double WHEEL_DIAMETER = 90; //Wheel diameter in mm
+    public final int MOTOR_GEAR_TEETH = 26; //# of teeth on the motor gear
+    public final int WHEEL_GEAR_TEETH = 20; //# of teeth on the wheel gear
+    public final double GEAR_RATIO = (MOTOR_GEAR_TEETH + 0.0) / WHEEL_GEAR_TEETH; //For every full turn of the motor, the wheel turns this many rotations.
+    public final double MM_TO_INCHES =  25.4;
+    public final double MOTOR_TO_INCHES = GEAR_RATIO * WHEEL_DIAMETER * Math.PI / MM_TO_INCHES; //For every full turn of both motors, the wheel moves forward this many inches
+    public final double NUMBER_OF_ENCODER_TICKS_PER_REVOLUTION = 537.6;
+
+    TankDriveHardware tank = new TankDriveHardware();
+    ElapsedTime time = new ElapsedTime();
+    ArrayList<Double> timeRecord = new ArrayList<>();
+    BNO055IMU imu;
+    double globalAngle;
+    Orientation lastAngles = new Orientation();
+
+
+    PIDController PID = new PIDController(0, 0, 0);
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        tank.init(hardwareMap);
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
+        parameters.mode                = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled      = false;
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        imu.initialize(parameters);
+
+        telemetry.addData("Mode", "calibrating...");
+        telemetry.update();
+
+        // make sure the imu gyro is calibrated before continuing.
+        /*while (!isStopRequested() && !imu.isGyroCalibrated())
+        {
+            sleep(50);
+            idle();
+        }*/
+
+        telemetry.addData("Mode", "waiting for start");
+        telemetry.addData("imu calib status", imu.getCalibrationStatus().toString());
+        telemetry.addData("Number of inches per motor rotation", MOTOR_TO_INCHES);
+        telemetry.update();
+
+        waitForStart();
+
+
+        while (opModeIsActive()) {
+            lastAngles = imu.getAngularOrientation();
+
+            telemetry.addData("Gyro Roll", lastAngles.firstAngle);
+            telemetry.update();
+
+
+        }
+
+    }
+}
