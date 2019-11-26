359d0dae8314a501c91c28b1c55765b73938b5ae
corn271828 <50471362+corn271828@users.noreply.github.com>
2019-11-26 15:11:18 -0600

    Update TankDriveTeleopClawerer.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveTeleopClawerer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveTeleopClawerer.java
@@ -12,33 +12,52 @@ public class TankDriveTeleopClawerer extends LinearOpMode {
     public void runOpMode() throws InterruptedException
     {
         tankDrive.init(hardwareMap);
-        tankDrive.LClaw.setPosition(0);
-        tankDrive.RClaw.setPosition(0);
-        tankDrive.PosClaw.setPosition(0);
-        telemetry.addData("All", "Done");
+        tankDrive.LClaw.setPosition(.5);
+        tankDrive.RClaw.setPosition(.25);
+        tankDrive.PosClaw.setPosition(.75);
+        telemetry.addData("R Claw :: ", tankDrive.RClaw.getPosition());
+        telemetry.addData("L Claw :: ", tankDrive.LClaw.getPosition());
+        telemetry.addData("Pos Claw :: ", tankDrive.PosClaw.getPosition());
         telemetry.update();
 
         waitForStart();
 
         while(opModeIsActive()) {
 
-            if (gamepad2.x)
-            {
-                tankDrive.LClaw.setPosition(.5);
-                tankDrive.RClaw.setPosition(.5);
+            if (gamepad2.dpad_right) {
+                tankDrive.RClaw.setPosition(tankDrive.RClaw.getPosition() + .1);
+                tankDrive.LClaw.setPosition(tankDrive.LClaw.getPosition() - .1);
+                telemetry.addData("R Claw CLAMP :: ", tankDrive.RClaw.getPosition());
+                telemetry.addData("L Claw CLAMP :: ", tankDrive.LClaw.getPosition());
+                telemetry.update();
+                sleep(100);
             }
-            if (gamepad2.y)
-            {
-                tankDrive.PosClaw.setPosition(.5);
+            else if (gamepad2.dpad_left) {
+                tankDrive.RClaw.setPosition(tankDrive.RClaw.getPosition() - .1);
+                tankDrive.LClaw.setPosition(tankDrive.LClaw.getPosition() + .1);
+                telemetry.addData("R Claw LETGO :: ", tankDrive.RClaw.getPosition());
+                telemetry.addData("L Claw LETGO :: ", tankDrive.LClaw.getPosition());
+                telemetry.update();
+                sleep(100);
+
+            }
+            else if (gamepad2.dpad_up) {
+                tankDrive.PosClaw.setPosition(tankDrive.PosClaw.getPosition() + .1);
+                telemetry.addData("Pos Claw TURN R :: ", tankDrive.PosClaw.getPosition());
+                telemetry.update();
+                sleep(100);
+
+            }
+            else if (gamepad2.dpad_down) {
+                tankDrive.PosClaw.setPosition(tankDrive.PosClaw.getPosition() - .1);
+                telemetry.addData("Pos Claw TURN L :: ", tankDrive.PosClaw.getPosition());
+                telemetry.update();
+                sleep(100);
+
             }
 
 
-            telemetry.addData("Left Claw:", tankDrive.LClaw.getPosition());
-            telemetry.addData("Right Claw:", tankDrive.RClaw.getPosition());
-            telemetry.addData("Pos Claw:", tankDrive.PosClaw.getPosition());
-            telemetry.update();
         }
-
         //freeze();
 
     }
