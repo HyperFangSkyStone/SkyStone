ff9cff2599f9f1f6effc2571331a27afc45730a9
corn271828 <50471362+corn271828@users.noreply.github.com>
2019-11-26 12:21:28 -0600

    Create TankDriveTeleopClawerer.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveTeleopClawerer.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Tank Clawerer", group="1")
+//@Disabled
+public class TankDriveTeleopClawerer extends LinearOpMode {
+
+    TankDriveALPHA tankDrive = new TankDriveALPHA();
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        tankDrive.init(hardwareMap);
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+            if (gamepad2.x)
+            {
+                tankDrive.LClaw.setPosition(.5);
+                tankDrive.RClaw.setPosition(.5);
+            }
+            if (gamepad2.y)
+            {
+                tankDrive.PosClaw.setPosition(.5);
+            }
+
+
+            telemetry.addData("Left Claw:", tankDrive.LClaw.getPosition());
+            telemetry.addData("Right Claw:", tankDrive.RClaw.getPosition());
+            telemetry.addData("Pos Claw:", tankDrive.PosClaw.getPosition());
+            telemetry.update();
+        }
+
+        //freeze();
+
+    }
+}
