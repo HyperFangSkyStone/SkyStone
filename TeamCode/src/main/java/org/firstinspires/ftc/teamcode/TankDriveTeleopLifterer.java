1b5c480c31146b39196185c389bcb6a2d4e541ef
corn271828 <50471362+corn271828@users.noreply.github.com>
2019-11-27 12:57:06 -0600

    Update TankDriveTeleopLifterer.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveTeleopLifterer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveTeleopLifterer.java
@@ -19,7 +19,7 @@ public class TankDriveTeleopLifterer extends LinearOpMode {
             if (gamepad2.y) {
                 int targ = 175; //Target
                 double kp = 0.003;
-                double powerFloor = 0.3;
+                double powerFloor = 0.35;
                 int errMarg = 10;
                 while (Math.abs(tankDrive.Lift1.getCurrentPosition() - targ) > errMarg && Math.abs(tankDrive.Lift1.getCurrentPosition() - targ) > errMarg && opModeIsActive()) {
                     int error = targ - Math.max(tankDrive.Lift1.getCurrentPosition(), tankDrive.Lift2.getCurrentPosition());
@@ -33,8 +33,11 @@ public class TankDriveTeleopLifterer extends LinearOpMode {
                     telemetry.addData("Porg:", porg);
                     telemetry.addData("Lift 1:", tankDrive.Lift1.getCurrentPosition());
                     telemetry.addData("Lift 2:", tankDrive.Lift2.getCurrentPosition());
+                    telemetry.addData("Error:", error);
                     telemetry.update();
                 }
+                tankDrive.Lift1.setPower(0);
+                tankDrive.Lift2.setPower(0);
             } else if (Math.abs(gamepad2.left_stick_y) > 0.1) {
                 tankDrive.Lift1.setPower(gamepad2.left_stick_y / 2);
                 tankDrive.Lift2.setPower(gamepad2.left_stick_y / 2);
