aaa06ae1155a6148acc4a0cf805dcc9a31f4910f
corn271828 <50471362+corn271828@users.noreply.github.com>
2019-12-02 16:03:22 -0600

    Update NewFoundationRedNoPark.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewFoundationRedNoPark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewFoundationRedNoPark.java
@@ -75,12 +75,6 @@ public class NewFoundationRedNoPark extends LinearOpMode {
             freeze();
             pidLinearMovement(35,2);
             freeze();
-            turnOneWheelDirection(-45, 0.8, 0.5, 0.005, 3);
-            sleep(200);
-            turnOneWheelDirection(45, 0.8,0.5, 0.005,3,'l');
-            sleep(200);
-            pidLinearMovement(-20,3);
-            freeze();
             break;
         }
     }
