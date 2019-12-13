32f64feaa1d5ec59461b9ed83de9b37c74c50510
Eric Y <ericyzt@hotmail.com>
2019-12-13 13:13:10 -0600

    Double Skystone Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoubleSkyStoneBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoubleSkyStoneBlue.java
@@ -10,9 +10,9 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.teamcode.DISABLED.VisionBitMapping;
 
 @SuppressWarnings("ALL")
-@Autonomous(name="DoubleSkyStoneTest", group="blue")
+@Autonomous(name="Double Skystone Blue", group="blue")
 //@Disabled
-public class TankDriveAutoBlue extends LinearOpMode {
+public class DoubleSkyStoneBlue extends LinearOpMode {
 
     TankDriveALPHA tankDrive = new TankDriveALPHA();
 
@@ -21,8 +21,6 @@ public class TankDriveAutoBlue extends LinearOpMode {
     PIDController linearPID = new PIDController(0, 0, 0);
     PIDController turningPID = new PIDController(0, 0, 0);
     Orientation lastAngles = new Orientation();
-
-    ElapsedTime clock = new ElapsedTime();
 
     public static int skystonePosition = 0;
     public static double distanceTimeIndex = 0; //time expected per inch
@@ -59,8 +57,6 @@ public class TankDriveAutoBlue extends LinearOpMode {
 
         //VisionBitMapping vbm = new VisionBitMapping(this);
 
-        tankDrive.RightNugget.setPosition(tankDrive.ROUT);
-        tankDrive.LeftNugget.setPosition(tankDrive.LOUT);
         while (!isStarted())
         {
             skystonePosition = 1;//vbm.skyStonePos('b');
@@ -73,47 +69,34 @@ public class TankDriveAutoBlue extends LinearOpMode {
             }
         }
 
+        tankDrive.RightNugget.setPosition(tankDrive.ROUT);
+        tankDrive.LeftNugget.setPosition(tankDrive.LOUT);
         waitForStart();
-        if(!isStopRequested())
+
+        while(isStarted())
         {
+            tankDrive.RightNugget.setPosition(tankDrive.RIN);
+            tankDrive.LeftNugget.setPosition(tankDrive.LIN);
             runIntake(1);
             sleep(750);
             runIntake(0);
             if (skystonePosition == 1)
             {
-                turnOneWheelDirection(8, 0.6, 0.4, 0.005, 1.5);
-                pidLinearMovement(20, 2.5);
+                movethForward(24, 2.5, 0.001, 0.5);
                 freeze();
-                pidLinearMovement(5,1);
-                runIntake(-1);
-                tankDrive.RightNugget.setPosition(tankDrive.RIN);
-                tankDrive.LeftNugget.setPosition(tankDrive.LIN);
-                sleep(500);
+                turnethDirection(10,0.35, 0.3, 0.005, 3);
+                runIntake(-0.5);
+                movethForward(26, 2.5, 0.0009, 0.3, 0.2);
+                sleep(250);
                 runIntake(0);
-                pidLinearMovement(-5, 1);
-                turnOneWheelDirection(82, 0.6, 0.4, 0.005, 2.5,'l');
-                pidLinearMovement(50, 3);
-                runIntake(1);
-                sleep(750);
+                movethForward(-36, 2.5, 0.001, 0.4);
+                runIntake(0.5);
+                sleep(110);
                 runIntake(0);
-                pidLinearMovement(-60, 3.5);
-                tankDrive.RightNugget.setPosition(tankDrive.ROUT);
-                tankDrive.LeftNugget.setPosition(tankDrive.LOUT);
-                turnOneWheelDirection(115, 0.6, 0.4, 0.005, 3);
-                pidLinearMovement(10, 1.5);
-                runIntake(-1);
-                tankDrive.RightNugget.setPosition(tankDrive.RIN);
-                tankDrive.LeftNugget.setPosition(tankDrive.LIN);
-                sleep(500);
-                runIntake(0);
-                turnOneWheelDirection(75, 0.6, 0.4, 0.005, 2.5, 'l');
-                pidLinearMovement(20, 2);
-                runIntake(1);
-                sleep(750);
-                runIntake(0);
-                turnOneWheelDirection(-60, 0.6, 0.4, 0.005, 2, 'l');
+                freeze();
+                turnethDirection(-100,0.4, 0.3, 0.005, 3);
             }
-            /*else if (skystonePosition == 2)
+            else if (skystonePosition == 2)
             {
                 turnOneWheelDirection(60, 0.5, 0.3, 0.003, 3);
                 turnethDirection(-60, 0.5, 0.3, 0.003, 3);
@@ -146,21 +129,21 @@ public class TankDriveAutoBlue extends LinearOpMode {
                 runIntake(0);
                 freeze();
                 turnethDirection(-65,0.35, 0.3, 0.005, 3);
-            }*/
+            }
             else
             {
                 telemetry.addData("ERROR", "SkyStone Not Found.");
                 telemetry.update();
                 freeze();
             }
-            /*
+
             skystonePosition = 0;
             runIntake(-1);
             movethForward(-40, 3.5, 0.001, 0.8, 0.3);
             freeze();
             runIntake(0);
             movethForward(15, 2, 0.001, 0.4, 0.25);
-            claw(true);
+            /*claw(true);
             movethForward(80, 2.5, 0.001, 0.6);
             turnethDirection(-90,0.4, 0.25, 0.005, 4);
             movethForward(30, 2.5, 0.001, 0.6);
@@ -199,124 +182,150 @@ public class TankDriveAutoBlue extends LinearOpMode {
         return floor + (ceiling - floor) / (1 + Math.pow(Math.E, stiff * (half - error)));
     }
 
-    public void pidLinearMovement(double distance, double timeframe)
+    public void movethForward(double inches, double t, double kp, double power)
     {
-        double conversionIndex = 537.6/((26.0/20.0)*90.0* Math.PI / 25.4); // Ticks per inch
-        double timeFrame = timeframe; //distance * distanceTimeIndex;
-        double errorMargin = 5;
-        double powerFloor = 0.25;
-        double powerCeiling = 0.8;
-
-        clock.reset();
         tankDrive.resetEncoders();
+        ElapsedTime clock = new ElapsedTime();
+        clock.reset();
+        double encoderTicks = inches / MOTOR_TO_INCHES * NUMBER_OF_ENCODER_TICKS_PER_REVOLUTION; // target number of encoder tick
+        int errorMargin = 10; //Amount of error in ticks we are willing to accept
+        double powerFloor = 0.19; //Minimum power
+        double powerCeiling = power; //Maximum power
 
-        double targetTick = distance / MOTOR_TO_INCHES * NUMBER_OF_ENCODER_TICKS_PER_REVOLUTION *50/47;
-        telemetry.addData("ticks", targetTick);
+        telemetry.addData("ticks", encoderTicks);
         telemetry.update();
-        double error = targetTick;
-        double errorPrev = 0;
-        double kP = 1.5;
-        double kD = 0.01;
-        double p, d;
-        double output;
-        double time = clock.seconds();
-        double timePrev = 0;
-
-
-        while (clock.seconds() < timeFrame && Math.abs(error) > errorMargin && opModeIsActive())
+        while (Math.abs(tankDrive.getAverageEncoder('l') + tankDrive.getAverageEncoder('r') - encoderTicks * 2) > errorMargin && clock.seconds() < t && opModeIsActive())
         {
-            //output = linearPID.PIDOutput(targetTick,averageEncoderTick(),clock.seconds());
+            //double leftpower = Math.abs(encoderTicks - tank.getAverageEncoder('l')) * kp;
+            double leftpower = sigmoid(encoderTicks - tankDrive.getAverageEncoder('l'), powerCeiling, powerFloor, 800, kp * 4);
+            leftpower = Math.max(leftpower, powerFloor);
+            leftpower = Math.min(leftpower, powerCeiling);
+            if (encoderTicks - tankDrive.getAverageEncoder('l') < 0) leftpower *= -1;
+            tankDrive.LM0.setPower(leftpower);
+            tankDrive.LM1.setPower(leftpower);
+            telemetry.addData("LeftPower", leftpower);
 
-            p = Math.abs(error/targetTick * kP);
-            d = 0; //((error - errorPrev) / (time - timePrev)) / 1000 /targetTick * kD;
+            //double rightpower = Math.abs(encoderTicks - tank.getAverageEncoder('r')) * kp;
+            double rightpower = leftpower;
+            tankDrive.RM0.setPower(rightpower);
+            tankDrive.RM1.setPower(rightpower);
 
-            output = p + d;
-            output = Math.max(output, powerFloor);
-            output = Math.min(output, powerCeiling);
-            if (error < 0) output *= -1;
-            runMotor(output, output);
-
-            errorPrev = error;
-
-            double tempAvg = targetTick > 0 ? tankDrive.getEncoderAvg(telemetry) : -tankDrive.getEncoderAvg(telemetry);
-            error = targetTick - tempAvg;
-
-            timePrev = time;
-            time = clock.seconds();
-
-            telemetry.addData("Target", targetTick);
-            telemetry.addData("Current", averageEncoderTick());
-            telemetry.addData("RM0", tankDrive.RM0.getCurrentPosition());
-            telemetry.addData("RM1", tankDrive.RM1.getCurrentPosition());
-            telemetry.addData("LM0", tankDrive.LM0.getCurrentPosition());
-            telemetry.addData("LM1", tankDrive.LM1.getCurrentPosition());
-            telemetry.addData("error", error);
-            telemetry.addData("kP", kP);
-            telemetry.addData("output", output);
+            telemetry.addData("RightPower", rightpower);
+            telemetry.addData("Left Encoder", tankDrive.getAverageEncoder('l'));
+            telemetry.addData("Right Encoder", tankDrive.getAverageEncoder('r'));
+            telemetry.addData("ticks", encoderTicks);
+            telemetry.addData("Current L", tankDrive.getAverageEncoder('l'));
+            telemetry.addData("Current R", tankDrive.getAverageEncoder('l'));
+            telemetry.addData("kP", kp);
             telemetry.update();
-
-
         }
-        runMotor(0,0);
+        freeze();
+        turnethDirection(0, 0.35, 0.2, 0.005, 3);
+    }
+    public void movethForward(double inches, double t, double kp, double ceil, double floor)
+    {
+        tankDrive.resetEncoders();
+        ElapsedTime clock = new ElapsedTime();
+        clock.reset();
+        double encoderTicks = inches / MOTOR_TO_INCHES * NUMBER_OF_ENCODER_TICKS_PER_REVOLUTION; // target number of encoder tick
+        int errorMargin = 10; //Amount of error in ticks we are willing to accept
+        double powerFloor = floor; //Minimum power
+        double powerCeiling = ceil; //Maximum power
+
+        telemetry.addData("ticks", encoderTicks);
+        telemetry.update();
+        while (Math.abs(tankDrive.getAverageEncoder('l') + tankDrive.getAverageEncoder('r') - encoderTicks * 2) > errorMargin && clock.seconds() < t && opModeIsActive())
+        {
+
+            //double leftpower = Math.abs(encoderTicks - tank.getAverageEncoder('l')) * kp;
+            double leftpower = sigmoid(encoderTicks - tankDrive.getAverageEncoder('l'), powerCeiling, powerFloor, 800, kp * 4);
+            leftpower = Math.max(leftpower, powerFloor);
+            leftpower = Math.min(leftpower, powerCeiling);
+            if (encoderTicks - tankDrive.getAverageEncoder('l') < 0) leftpower *= -1;
+            tankDrive.LM0.setPower(leftpower);
+            tankDrive.LM1.setPower(leftpower);
+            telemetry.addData("LeftPower", leftpower);
+
+            //double rightpower = Math.abs(encoderTicks - tank.getAverageEncoder('r')) * kp;
+            double rightpower = leftpower;
+            tankDrive.RM0.setPower(rightpower);
+            tankDrive.RM1.setPower(rightpower);
+
+            telemetry.addData("Left Encoder", tankDrive.getAverageEncoder('l'));
+            telemetry.addData("Right Encoder", tankDrive.getAverageEncoder('r'));
+            telemetry.addData("ticks", encoderTicks);
+            telemetry.addData("Current L", tankDrive.getAverageEncoder('l'));
+            telemetry.addData("Current R", tankDrive.getAverageEncoder('l'));
+            telemetry.addData("kP", kp);
+            telemetry.update();
+        }
+        freeze();
+
+        turnethDirection(0, 0.35, 0.2, 0.005, 3);
     }
 
-    public void overshootLinearMovement(double distance, double timeframe)
+
+    private void turnethDirection(int initial, double powerCeiling, double powerFloor, double kp, double t)
     {
-        double conversionIndex = 537.6/((26.0/20.0)*90.0* Math.PI / 25.4); // Ticks per inch
-        double timeFrame = timeframe; //distance * distanceTimeIndex;
-        double errorMargin = 5;
-        double powerFloor = 0.2;
-        double powerCeiling = 0.8;
-
+        lastAngles = imu.getAngularOrientation();
+        double currentAngle = lastAngles.firstAngle;
+        ElapsedTime clock = new ElapsedTime();
         clock.reset();
-        tankDrive.resetEncoders();
+        globalAngle += initial;
+        if(globalAngle > 180)
+            globalAngle -=360;
+        if (globalAngle < -180)
+            globalAngle += 360;
+        double  leftPower, rightPower;
 
-        double targetTick = distance / MOTOR_TO_INCHES * NUMBER_OF_ENCODER_TICKS_PER_REVOLUTION *50/47;
-        telemetry.addData("ticks", targetTick);
+        // restart imu movement tracking.
+        //resetAngle();
+
+        telemetry.addData("currentAngle", currentAngle);
         telemetry.update();
-        double error = targetTick;
-        double errorPrev = 0;
-        double kP = 0.8;
-        double kD = 0.01;
-        double p, d;
-        double output;
-        double time = clock.seconds();
-        double timePrev = 0;
+        // getAngle() returns + when rotating counter clockwise (left) and - when rotating
+        // clockwise (right).
 
+        // set power to rotate.
 
-        while (clock.seconds() < timeFrame && error > 0 && opModeIsActive())
+        // rotate until turn is completed.
+        while (clock.seconds() < t && Math.abs(globalAngle - imu.getAngularOrientation().firstAngle) > 2 && opModeIsActive())
         {
-            //output = linearPID.PIDOutput(targetTick,averageEncoderTick(),clock.seconds());
+            lastAngles = imu.getAngularOrientation();
+            currentAngle = lastAngles.firstAngle;
 
-            p = Math.abs(error)/targetTick * kP;
-            d = ((error - errorPrev) / (time - timePrev)) /targetTick * kD;
-
-            output = p + d;
-            output = Math.max(output, powerFloor);
-            output = Math.min(output, powerCeiling);
-            if (error < 0) output *= -1;
-            tankDrive.runMotor(output, output);
-
-            errorPrev = error;
-            error = targetTick - tankDrive.getEncoderAvg(telemetry);
-
-            timePrev = time;
-            time = clock.seconds();
-
-            telemetry.addData("Target", targetTick);
-            telemetry.addData("Current", averageEncoderTick());
-            telemetry.addData("RM0", tankDrive.RM0.getCurrentPosition());
-            telemetry.addData("RM1", tankDrive.RM1.getCurrentPosition());
-            telemetry.addData("LM0", tankDrive.LM0.getCurrentPosition());
-            telemetry.addData("LM1", tankDrive.LM1.getCurrentPosition());
-            telemetry.addData("error", error);
-            telemetry.addData("kP", kP);
-            telemetry.addData("output", output);
+            telemetry.addData("CurrentAngle", currentAngle);
             telemetry.update();
 
+            double error = globalAngle - currentAngle;
+            if (error > 180)
+                error -= 360;
+            if (error < -180)
+                error += 360;
+            double rawPower = Math.abs(error) * kp;
+            rawPower = Math.max(rawPower, powerFloor);
+            rawPower = Math.min(rawPower, powerCeiling);
+            leftPower = rawPower;
+            rightPower = rawPower;
+            if (error > 0)
+                leftPower *= -1;
+            else
+                rightPower *= -1;
 
+            tankDrive.LM0.setPower(leftPower);
+            tankDrive.LM1.setPower(leftPower);
+            tankDrive.RM0.setPower(rightPower);
+            tankDrive.RM1.setPower(rightPower);
         }
-        tankDrive.runMotor(0,0);
+
+        // turn the motors off.
+        tankDrive.LM0.setPower(0);
+        tankDrive.LM1.setPower(0);
+        tankDrive.RM0.setPower(0);
+        tankDrive.RM1.setPower(0);
+
+        // wait for rotation to stop.
+        sleep(1000);
     }
 
     private void turnOneWheelDirection(int initial, double powerCeiling, double powerFloor, double kp, double t)
@@ -361,14 +370,15 @@ public class TankDriveAutoBlue extends LinearOpMode {
             rawPower = Math.min(rawPower, powerCeiling);
             leftPower = rawPower;
             rightPower = rawPower;
-
-            if (error < 0)
+            if (error > 0)
                 leftPower *= -1;
+            else
+                rightPower *= -1;
 
-            tankDrive.RM0.setPower(leftPower);
-            tankDrive.RM1.setPower(leftPower);
-            //tankDrive.RM0.setPower(rightPower);
-            //tankDrive.RM1.setPower(rightPower);
+            //tankDrive.LM0.setPower(leftPower);
+            //tankDrive.LM1.setPower(leftPower);
+            tankDrive.RM0.setPower(rightPower);
+            tankDrive.RM1.setPower(rightPower);
         }
 
         // turn the motors off.
@@ -404,73 +414,5 @@ public class TankDriveAutoBlue extends LinearOpMode {
             tankDrive.LeftFang.setPosition(0); //false = down
             tankDrive.RightFang.setPosition(1);
         }
-    }
-
-    private void turnOneWheelDirection(int initial, double powerCeiling, double powerFloor, double kp, double t, char l)
-    {
-        lastAngles = imu.getAngularOrientation();
-        double currentAngle = lastAngles.firstAngle;
-        ElapsedTime clock = new ElapsedTime();
-        clock.reset();
-        globalAngle += initial;
-        if(globalAngle > 180)
-            globalAngle -=360;
-        if (globalAngle < -180)
-            globalAngle += 360;
-        double  leftPower, rightPower;
-
-        // restart imu movement tracking.
-        //resetAngle();
-
-        telemetry.addData("currentAngle", currentAngle);
-        telemetry.update();
-        // getAngle() returns + when rotating counter clockwise (left) and - when rotating
-        // clockwise (right).
-
-        // set power to rotate.
-
-        // rotate until turn is completed.
-        while (clock.seconds() < t && Math.abs(globalAngle - imu.getAngularOrientation().firstAngle) > 3 && opModeIsActive())
-        {
-            lastAngles = imu.getAngularOrientation();
-            currentAngle = lastAngles.firstAngle;
-
-            telemetry.addData("CurrentAngle", currentAngle);
-            telemetry.update();
-
-            double error = globalAngle - currentAngle;
-            if (error > 180)
-                error -= 360;
-            if (error < -180)
-                error += 360;
-            double rawPower = Math.abs(error) * kp;
-            rawPower = Math.max(rawPower, powerFloor);
-            rawPower = Math.min(rawPower, powerCeiling);
-            leftPower = rawPower;
-            rightPower = rawPower;
-
-            if (error < 0)
-                leftPower *= -1;
-
-            if (l == 'l') {
-                tankDrive.LM0.setPower(-leftPower);
-                tankDrive.LM1.setPower(-leftPower);
-            }
-            else {
-                tankDrive.RM0.setPower(leftPower);
-                tankDrive.RM1.setPower(leftPower);
-            }
-            //tankDrive.RM0.setPower(rightPower);
-            //tankDrive.RM1.setPower(rightPower);
-        }
-
-        // turn the motors off.
-        tankDrive.LM0.setPower(0);
-        tankDrive.LM1.setPower(0);
-        tankDrive.RM0.setPower(0);
-        tankDrive.RM1.setPower(0);
-
-        // wait for rotation to stop.
-        sleep(1000);
     }
 }
