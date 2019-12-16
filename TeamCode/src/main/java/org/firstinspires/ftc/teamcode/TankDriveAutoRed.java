c79208934d70ced5cb5885d7a775c7ba45432451
corn271828 <50471362+corn271828@users.noreply.github.com>
2019-12-16 08:30:40 -0600

    Update TankDriveAutoRed.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveAutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDriveAutoRed.java
@@ -7,7 +7,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-import org.firstinspires.ftc.teamcode.DISABLED.VisionBitMapping;
 
 @SuppressWarnings("ALL")
 @Autonomous(name="Skystone Red", group="red")
