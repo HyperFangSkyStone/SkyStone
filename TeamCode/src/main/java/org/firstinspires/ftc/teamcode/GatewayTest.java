f947f18a100cc65a2858eda9124e2f6da082eb47
corn271828 <50471362+corn271828@users.noreply.github.com>
2020-01-17 17:25:46 -0600

    Update GatewayTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GatewayTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GatewayTest.java
@@ -52,7 +52,7 @@ public class GatewayTest extends LinearOpMode {
 
         while (opModeIsActive()) {
             telemetry.addData("RightGate:", tankDrive.RightGate.getPosition());
-            telemetry.addData("LeftGate:", tankDrive.LeftGate.getPosition());
+            //telemetry.addData("LeftGate:", //tankDrive.LeftGate.getPosition());
             telemetry.addData("Pusher:", tankDrive.Pusher.getPosition());
             telemetry.addData("Vlad:", tankDrive.VladTheImpaler.getPosition());
             telemetry.update();
