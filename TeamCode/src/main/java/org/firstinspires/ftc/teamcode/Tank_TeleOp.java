46593837f5aa3923d7cf0ab5a02bea946ce9cfae
corn271828 <50471362+corn271828@users.noreply.github.com>
2020-01-31 16:03:33 -0600

    Update Tank_TeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tank_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tank_TeleOp.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -7,7 +8,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import java.util.ArrayList;
 
 @TeleOp(name="ChimpanzeeAutoPIDTest", group="1")
-//@Disabled
+@Disabled
 public class Tank_TeleOp extends LinearOpMode {
 
     TankDriveALPHA tankDrive = new TankDriveALPHA();
