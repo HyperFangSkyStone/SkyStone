789dfef605d1128e2a759bf83b05e83d15d88559
corn271828 <50471362+corn271828@users.noreply.github.com>
2020-01-31 16:03:36 -0600

    Update SeonsorsTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SeonsorsTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SeonsorsTest.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -10,7 +11,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import java.util.ArrayList;
 
 @TeleOp(name="SEONOSORES TEST", group="1")
-//@Disabled
+@Disabled
 public class SeonsorsTest extends LinearOpMode {
 
     TankDriveALPHA tankDrive = new TankDriveALPHA();
