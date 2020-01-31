fde871cef4d68fc23eed36156cf846f01d2fe208
corn271828 <50471362+corn271828@users.noreply.github.com>
2020-01-31 16:06:54 -0600

    Update FoundationBlueCoop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FoundationBlueCoop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FoundationBlueCoop.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -10,7 +11,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @SuppressWarnings("ALL")
 @Autonomous(name="Foundation Blue Coop", group="blue")
-//@Disabled
+@Disabled
 public class FoundationBlueCoop extends LinearOpMode {
 
     TankDriveALPHA tankDrive = new TankDriveALPHA();
